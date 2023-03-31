b4543cab7e45a5f6a8f82562bfc84599fe6c0dab
elsapet <elizabeth@bearer.sh>
2023-03-31 15:09:50 +0200

    WIP

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 #
 # Run the rules test suits
 
@@ -9,26 +9,25 @@ N=10
 TEST_COUNT=0
 FAILURES=0
 
-i=0
 for dir in $(find $PWD -type d -name "testdata"); do
-  i=$((i+1))
-  # (
-    # rule_id=${dir//$PWD\//}
-    # rule_id=${rule_id///testdata/}
-    # rule_id=${rule_id//\//_}
+  (
+    rule_id="${dir//$PWD\//}"
+    rule_id="${rule_id///testdata/}"
+    rule_id="${rule_id//\//_}"
+
+    printf "$rule_id\n"
 
     for file in $(find $dir -type f); do
       TEST_COUNT=$((TEST_COUNT+1))
 
       test_result=$(mktemp "${TMPDIR:-/tmp/}$(basename $file).XXXXXXXXXXXX")
-      test_snapshot=${file//testdata/.snapshots}.yml
+      test_snapshot="${file//testdata/.snapshots}"
 
       printf "$test_snapshot\n"
 
-    #   # docker run --platform linux/amd64 --rm -v $dir:/tmp/scan/$dir bearer/bearer:latest-amd64 scan /tmp/scan$file --only-rule=$rule_id --format=yaml > $test_result
-    #   docker run --platform linux/amd64 --rm -v $dir:/tmp/scan/$dir bearer/bearer:latest-amd64 scan /tmp/scan$file --format=yaml > $test_result
+      # docker run --platform linux/amd64 --rm -v $dir:/tmp/scan/$dir bearer/bearer:latest-amd64 scan /tmp/scan$file --only-rule=$rule_id --disable-default-rules=true --format=yaml > $test_result
 
-      if [ -n "$UPDATE_SNAPSHOTS" ] || [ ! -f $test_snapshot ]
+      if [ -n "$UPDATE_SNAPSHOTS" ] # || [ ! -f $test_snapshot ]
       then
         printf "INFO: Building snapshot...\n"
         cat $test_result > $test_snapshot
@@ -41,13 +40,15 @@ for dir in $(find $PWD -type d -name "testdata"); do
         fi
       fi
     done
-  # ) &
+  ) &
 
-  # (( $i % $N == 0 )) && wait
-
+  if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+      # job limit reached ; wait until some job finishes
+      wait -n
+  fi
 done
 
-# wait
+wait
 
 printf "INFO: $TEST_COUNT tests run\n"
 if [ $FAILURES != 0 ]
