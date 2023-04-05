defa4ef04165f405a91128d5d7782f74dea16894
gotbadger <p.j.h@hey.com>
2023-04-05 13:06:25 +0100

    feat: alter loading and snapshots

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,50 +6,48 @@ printf "INFO: Running tests...\n"
 
 TEST_COUNT=0
 FAILURES=0
+TARGET=$1
 
 trap printexit SIGINT
 printexit() {
   printf "INFO: $TEST_COUNT tests run\n"
-  if [ $FAILURES != 0 ]
-    exit 0
-  then
+  if ((  $FAILURES != 0 )); then
     printf "ERROR: $FAILURES tests failed\n"
     exit 1
   fi
+  printf "INFO: tests passed ok!\n"
+  exit 0
 }
 
-for dir in $(find $PWD -type d -name "testdata"); do
-  (
-    rule_id="${dir//$PWD\//}"
-    rule_id="${rule_id///testdata/}"
-    rule_id="${rule_id//\//_}"
+for dir in $(find ${PWD}/$TARGET -type d -name "testdata"); do
+  rule_id="${dir//$PWD\//}"
+  rule_id="${rule_id///testdata/}"
+  rule_id="${rule_id//\//_}"
+  printf "$rule_id\n"
 
-    printf "$rule_id\n"
+  for file in $(find $dir -type f); do
+    TEST_COUNT=$((TEST_COUNT+1))
 
-    for file in $(find $dir -type f); do
-      TEST_COUNT=$((TEST_COUNT+1))
+    test_result=$(mktemp "${TMPDIR:-/tmp/}$(basename $file).XXXXXXXXXXXX")
+    test_snapshot="${file//testdata/.snapshots}"
+    test_snapshot=${test_snapshot%.*}.yml
+    filename=$(basename $file)
 
-      test_result=$(mktemp "${TMPDIR:-/tmp/}$(basename $file).XXXXXXXXXXXX")
-      test_snapshot="${file//testdata/.snapshots}"
+    docker run --platform linux/amd64 --rm -v $dir:/tmp/scan -v $PWD:/tmp/rules bearer/bearer:canary-amd64 scan /tmp/scan/$filename --only-rule=$rule_id --disable-default-rules=true --external-rule-dir=/tmp/rules --format=yaml > $test_result
 
-      printf "$test_snapshot\n"
+    if [ -n "$UPDATE_SNAPSHOTS" ] || [ ! -f $test_snapshot ]
+    then
+      printf "INFO: Building snapshot...\n"
+      cat $test_result > $test_snapshot
+    else
+      diff $test_result $test_snapshot
 
-      docker run --platform linux/amd64 --rm -v $dir:/tmp/scan/$dir bearer/bearer:latest-amd64 scan /tmp/scan$file --only-rule=$rule_id --disable-default-rules=true --format=yaml > $test_result
-
-      if [ -n "$UPDATE_SNAPSHOTS" ] # || [ ! -f $test_snapshot ]
+      if [ $? -ne 0 ]
       then
-        printf "INFO: Building snapshot...\n"
-        cat $test_result > $test_snapshot
-      else
-        diff $test_result $test_snapshot
-
-        if [ $? -ne 0 ]
-        then
-          FAILURES=$((FAILURES+1))
-        fi
+        FAILURES=$((FAILURES+1))
       fi
-    done
-  )
+    fi
+  done
 done
 
-printexit()
+printexit
