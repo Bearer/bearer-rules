504d117e2d197c34d5f002c5230f39adcc0f9588
Philip Hayton <gotbadger@users.noreply.github.com>
2023-04-11 14:06:32 +0100

    ci: lint rule schema (#19)

---
+++ scripts/validate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+printf "INFO: Validating rule structure...\n"
+args=""
+for rule_file in $(find $PWD -type f | grep .yml | grep -vE '.snapshots|.github'); do
+  args="$args -d $rule_file"
+done
+
+npx ajv-cli -s scripts/rule_schema.json$args --allow-union-types
