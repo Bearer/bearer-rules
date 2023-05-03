bc6b6d3c47061abed02ea85a6171bbf7d82f5cae
vjerci <27707350+vjerci@users.noreply.github.com>
2023-05-03 15:21:30 +0200

    test: regenerate snapshots

--- java/lang/logger/testdata/simple.java
+++ java/lang/logger/testdata/simple.java
@@ -1,3 +1,18 @@
-User user;
-String name = user.name;
-logger.error(name);
+package servlets;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Cls extends HttpServlet
+{
+
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+    {
+            User user;
+            logger.error(user.name);
+    }
+}
