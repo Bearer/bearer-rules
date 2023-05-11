284f4c61224fc35b4dd5875e3615a7d8004daee4
David Roe <didroe@gmail.com>
2023-05-11 09:47:40 +0100

    feat: add template libaries to javascript raw html rule (#51)

--- javascript/lang/raw_html_using_user_input/testdata/bad.js
+++ javascript/lang/raw_html_using_user_input/testdata/bad.js
@@ -1,2 +1,35 @@
 `<h1 class="test">${req.params.oops}</h1>`
 `<h1>${req.params.oops}</h1>`
+
+doT.compile(req.params.oops, {})
+
+ejs.compile(req.params.oops, {})
+
+Handlebars.compile(req.params.oops, {})
+
+Hogan.parse(Hogan.scan(req.params.oops), null)
+
+$.templates(req.params.oops)
+
+template(req.params.oops, {})
+_.template(req.params.oops, {})
+
+compiler.compileSync(req.params.oops, "test.js")
+
+Mustache.render(req.params.oops, {})
+
+nunjucks.renderString(req.params.oops, {})
+
+pug.render(req.params.oops, {})
+
+Sqrl.render(req.params.oops, {})
+
+Template7.compile(req.params.oops, {})
+
+webix.ui({
+  id: "mylayout",
+  rows: [
+    { view: "toolbar", id: "mybar" },
+    { template: req.params.oops }
+  ]
+})
