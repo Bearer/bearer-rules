6825306c7ffb934e213b3520f4ba3ceb3088812e
elsapet <elizabeth@bearer.sh>
2023-05-24 12:21:52 +0200

    feat: match on any permitted risky param, regardless of use

--- ruby/rails/unsafe_mass_assignment/testdata/ok.rb
+++ ruby/rails/unsafe_mass_assignment/testdata/ok.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController
-  def create
-    user_params = params(:user).permit!(:name, :email, :company_id)
-    User.new(user_params)
+  def user_params
+    params(:user).permit!(:name, :email, :company_id)
   end
 end
--- ruby/rails/unsafe_mass_assignment/testdata/unsafe.rb
+++ ruby/rails/unsafe_mass_assignment/testdata/unsafe.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController
-  def create
-    user_params = params(:user).permit!(:name, :email, :admin)
-    User.new(user_params)
+  def user_params
+    params(:user).permit!(:name, :email, :admin)
   end
 end
