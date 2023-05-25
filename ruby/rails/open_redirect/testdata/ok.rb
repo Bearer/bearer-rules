1acfbef73209aecc3e8de0f4542daa02aa0b9935
elsapet <elizabeth@bearer.sh>
2023-05-25 09:33:35 +0200

    fix: extend rails open redirect rule

--- ruby/rails/open_redirect/testdata/ok.rb
+++ ruby/rails/open_redirect/testdata/ok.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def notify
     redirect_to some_route_url(params[:id]), status: :found
   end
+
+  def orders
+    redirect_to Rails.application.routes.url_helpers.orders_path(shop_id: params[:shop_id])
+  end
 end
