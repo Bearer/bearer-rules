985bc1f0cea1859332ad3a1d471a694f2aaa331f
elsapet <elizabeth@bearer.sh>
2023-05-26 17:46:08 +0200

    fix: fix Rails SQL injection pattern

--- ruby/rails/sql_injection/testdata/ok_using_bind.rb
+++ ruby/rails/sql_injection/testdata/ok_using_bind.rb
@@ -6,3 +6,9 @@ User.find_sole_by(["attr = ?", params[:ok]])
 ActiveRecord::Base.connection.exec_query("SELECT ?", "test", [[nil, params[:ok]]])
 
 connection.select_all("SELECT ?", "test", [[nil, params[:ok]]])
+
+search_params = {
+  name: params[:name],
+  organization_id: params[:organization_id]
+}
+Project.find_by(search_params)
