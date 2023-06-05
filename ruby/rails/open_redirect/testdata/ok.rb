89a83c65542a9174ca9a5a4c4a24f623738930c5
Cédric Fabianski <cfabianski@me.com>
2023-06-05 15:26:02 +0200

    feat: add sanitized patterns

--- ruby/rails/open_redirect/testdata/ok.rb
+++ ruby/rails/open_redirect/testdata/ok.rb
@@ -6,4 +6,10 @@ class OrdersController < ApplicationController
   def orders
     redirect_to Rails.application.routes.url_helpers.orders_path(shop_id: params[:shop_id])
   end
+
+  def show
+    id = params[:user_id].to_i
+
+    redirect_to organization.users.where(id: id)
+  end
 end
