34a9c90dedac72b48caa651e5d8c4a31fd5636c7
Karthik Thotta Ganesh <tg@karthiks-imac.lan>
2023-06-04 20:52:30 -0700

    Adding rule for log injection

---
+++ java/lang/log_injection/testdata/log_injection.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Log extends HttpServlet
+{
+
+    public void logInjection(HttpServletRequest request, HttpServletResponse response)
+    {
+            log = Logger.getLogger(Cls.class);
+            String username = request.getParameter("username");
+            log.info("Username is" + request.getParameter("username"));
+            log.warn("Username is" + username);
+            log.error(username);
+            log.error("This is not finding");
+
+          
+
+    }
+}
