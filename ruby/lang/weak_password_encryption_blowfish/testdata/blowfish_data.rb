4da73173af149e72764772b3b550abe42dcbe862
elsapet <elizabeth@bearer.sh>
2023-06-06 09:36:23 +0200

    fix: clean up snapshots

--- ruby/lang/weak_password_encryption_blowfish/testdata/blowfish_data.rb
+++ ruby/lang/weak_password_encryption_blowfish/testdata/blowfish_data.rb
@@ -6,4 +6,3 @@ Crypt::Blowfish.new("insecure").encrypt_block do |user|
   user.password
 end
 
-Crypt::Blowfish.new("your-key").encrypt_string(user.email)
--- ruby/lang/weak_password_encryption_blowfish/testdata/ok_not_a_password.rb
+++ ruby/lang/weak_password_encryption_blowfish/testdata/ok_not_a_password.rb
@@ -3,3 +3,4 @@ blowfish.encrypt_block do
   "hello #{current_user.full_name}"
 end
 
+Crypt::Blowfish.new("your-key").encrypt_string(user.email)
