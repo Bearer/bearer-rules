05802913b6ef9781feabf11e0e0c9a9bf40dbf66
Cédric Fabianski <cfabianski@me.com>
2023-06-08 17:00:37 +0200

    chore: wip

--- javascript/lang/dangerous_insert_html/testdata/secure.js
+++ javascript/lang/dangerous_insert_html/testdata/secure.js
@@ -2,11 +2,6 @@ function renderListItem(input) {
   this.ref.insertAdjacentHTML("beforebegin", `<li>fixed list item</li>`)
 }
 
-function ok1() {
-  const name = "<div>it's ok</div>"
-  el.innerHTML = name
-}
-
 function ok2() {
   const name = "<div>it's ok</div>"
   el.innerHTML = name
