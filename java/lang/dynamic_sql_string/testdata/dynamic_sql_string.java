354e418f7f949a1e0a67df6de451f75c77145982
elsapet <elizabeth@bearer.sh>
2023-06-14 09:46:18 +0200

    feat(java): add rule for dynamic SQL strings

---
+++ java/lang/dynamic_sql_string/testdata/dynamic_sql_string.java
@@ -0,0 +1,7 @@
+import java.sql.*;
+
+public class SQLExample {
+  public static void main(String tableName) {
+    String sqlQuery = String.format("SELECT * FROM ", tableName);
+  }
+}
---
+++ java/lang/dynamic_sql_string/testdata/ok.java
@@ -0,0 +1,8 @@
+import java.sql.*;
+
+public class SQLExample {
+  public static void main(String tableName) {
+    final String staticStr = "SELECT * FROM " + tableName
+    String sqlQuery = String.format(staticStr);
+  }
+}
