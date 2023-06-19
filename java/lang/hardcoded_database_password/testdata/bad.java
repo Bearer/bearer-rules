fd12621cacdb1d110aa1ccbaddb6d20cee0f6b3a
elsapet <elizabeth@bearer.sh>
2023-06-19 12:20:04 +0200

    feat(java): add hardcoded database password rule (#111)

---
+++ java/lang/hardcoded_database_password/testdata/bad.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Foo
+{
+  public static void main() {
+    String url = "jdbc:mysql://localhost:3306/foo";
+    Connection conn = DriverManager.getConnection(url, "admin", "admin");
+  }
+}
