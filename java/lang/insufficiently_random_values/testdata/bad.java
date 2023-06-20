1b18f88bee1b502809896d402f0355c4b95e9769
David Roe <didroe@gmail.com>
2023-06-20 15:12:31 +0100

    feat(java): add insufficiently random values rule (#113)
    Co-authored-by: elsapet <elizabeth@bearer.sh>

---
+++ java/lang/insufficiently_random_values/testdata/bad.java
@@ -0,0 +1,8 @@
+Random rand = new Random();
+rand.next();
+
+org.apache.commons.lang.RandomStringUtils.random(10);
+RandomStringUtils.random(10);
+
+org.apache.commons.lang.math.RandomUtils.nextint();
+RandomUtils.nextint();
