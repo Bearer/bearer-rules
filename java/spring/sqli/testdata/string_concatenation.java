080a79f473eece4daeb84bf7ca3acb1c56ff4509
elsapet <elizabeth@bearer.sh>
2023-06-23 11:54:43 +0200

    revert: "fix(java): make sql injection rule stricter (#121)"
    This reverts commit 2d8f5361925d41e3b8b680f20de139159e263ead.

--- java/spring/sqli/testdata/string_concatenation.java
+++ java/spring/sqli/testdata/string_concatenation.java
@@ -1,20 +1,8 @@
-package com.example.sqli;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.core;
 
 public class Foo {
-   @Autowired
-   JdbcTemplate t;
-
-   public void updateUser() {
+   public Bar updateUser(HttpServletRequest request, HttpServletResponse response) {
       String myQueryStr = "update user set name='"+request.getParameter("name")+"' where id='"+request.getParameter("userId")+"'";
-
-      var factory = new PreparedStatementCreatorFactory(myQueryStr, Types.VARCHAR);
-      factory.newPreparedStatementCreator(myQueryStr, []);
-
-      t.batchUpdate("ok", myQueryStr);
-      t.execute(myQueryStr);
+      JdbcTemplate.update(myQueryStr);
    }
 }
