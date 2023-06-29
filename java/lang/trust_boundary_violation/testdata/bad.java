c1eef8a61044e03fb744ea00c8663c8f1c07db48
David Roe <didroe@gmail.com>
2023-06-29 14:13:51 +0100

    fix(java): improvements from reviewing BenchmarkJava - part 1 (#133)

--- java/lang/trust_boundary_violation/testdata/bad.java
+++ java/lang/trust_boundary_violation/testdata/bad.java
@@ -1,10 +1,44 @@
+// This file is indirectly testing java_shared_lang_instance
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
 public class Main {
   private final static String KEY = "foo";
+  javax.servlet.ServletRequest field;
 
-  public static void main(HttpServletRequest request) {
+  public Main(HttpServletRequest constructor, String foo) {
+    constructor.setAttribute(KEY, getFoo());
+  }
+
+  public static void main(HttpServletRequest param) {
     var foo = getFoo();
 
-    request.setAttribute(KEY, foo);
-    request.setAttribute(foo, "value");
+    param.setAttribute(KEY, foo);
+    param.setAttribute(foo, "value");
+
+    field.setAttribute(KEY, getFoo());
+
+    javax.servlet.http.HttpServletRequest local = getReq();
+    local.setAttribute(KEY, getFoo());
+
+    // Fallback
+    request.setAttribute(KEY, getFoo());
+
+    // This is contrived, but here to give full coverage of the shared instance rule
+    try {
+      x.foo();
+    } catch (SomeException e) {
+      foo;
+    } catch (OtherException|HttpServletRequest e) {
+      e.setAttribute(KEY, getFoo());
+    }
+
+    try (ServletRequest resource = getReq()) {
+      resource.setAttribute(KEY, getFoo());
+    }
+
+    for (ServletRequest forEach : getReqs()) {
+      forEach.setAttribute(KEY, getFoo());
+    }
   }
 }
