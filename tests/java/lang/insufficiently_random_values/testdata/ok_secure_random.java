1ed8a41019d2a43811abdc123b05ef8e46ff7ee5
elsapet <elizabeth@bearer.sh>
2023-07-28 17:08:24 +0200

    fix: Java Random rule pattern

--- tests/java/lang/insufficiently_random_values/testdata/ok_secure_random.java
+++ tests/java/lang/insufficiently_random_values/testdata/ok_secure_random.java
@@ -10,10 +10,19 @@ public class Main
 		byte[] randomBytes = new byte[128];
 		secureRandomGenerator.nextBytes(randomBytes);
 
-		//Get random integer
+		// Get random integer
 		int r = secureRandomGenerator.nextInt();
 
-		//Get random integer in range
+		// Get random integer in range
 		int randInRange = secureRandomGenerator.nextInt(999999);
+
+		// sneaky example
+		java.util.Random numGen = java.security.SecureRandom.getInstance("SHA1PRNG");
+    double rand = getNextNumber(numGen);
+    String key = Double.toString(rand).substring(2);
 	}
+
+  double getNextNumber(java.util.Random generator) {
+    return generator.nextDouble();
+  }
 }
