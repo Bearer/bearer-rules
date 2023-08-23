475c3f2f8f15e210f411d2c17f24a40216e39887
Cédric Fabianski <cfabianski@me.com>
2023-08-23 09:51:31 +0200

    fix: improve sanitizer for react inner html

--- tests/javascript/react/dangerously_set_inner_html/test.js
+++ tests/javascript/react/dangerously_set_inner_html/test.js
@@ -3,35 +3,34 @@ const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
 
 describe(ruleId, () => {
   const invoke = createInvoker(ruleId, ruleFile, testBase)
-  
 
   test("insecure-raw_input", () => {
     const testCase = "insecure-raw_input.js"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
 
   test("insecure-template_string", () => {
     const testCase = "insecure-template_string.js"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
 
   test("secure-render-markdown", () => {
     const testCase = "secure-render-markdown.js"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
 
   test("secure-sanitize", () => {
     const testCase = "secure-sanitize.js"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
 
   test("secure-template_string", () => {
     const testCase = "secure-template_string.js"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
+
+  test("secure-dom-purify", () => {
+    const testCase = "secure-dom_purify.tsx"
+    expect(invoke(testCase)).toMatchSnapshot()
+  })
 })
