99b4cd69b321a748f5078ba2946c1c4feb760af0
Cédric Fabianski <cfabianski@me.com>
2023-09-18 11:43:33 +0200

    chore(php): improve logger tests

--- tests/php/third_parties/logger/testdata/insecure.php
+++ tests/php/third_parties/logger/testdata/insecure.php
@@ -1,5 +1,6 @@
 <?php
 
 $user = array('email' => 'user@email.com', 'uuid' => 'unique-identifier');
-$log->info('Adding a new user', $user.email);
+$log->info('Adding a new user', $user->email);
 $log->info('Adding a new user', $user["email"]);
+$log->info('Adding a new user', $user);
