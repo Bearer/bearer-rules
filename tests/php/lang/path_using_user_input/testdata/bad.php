925dce544fd49869ee08f5ff5af41e5642c24a27
David Roe <dave.roe@bearer.sh>
2023-09-22 13:38:27 +0100

    feat: path using user input rule

---
+++ tests/php/lang/path_using_user_input/testdata/bad.php
@@ -0,0 +1,65 @@
+<?php
+
+$oops = "/files/" . $_GET["oops"];
+
+// filesystem
+chgrp($oops, 1);
+chmod($oops, 0755);
+chown($oops, 1);
+copy($oops, $ok, null);
+copy($ok, $oops);
+disk_free_space($oops);
+diskfreespace($oops);
+disk_total_space($oops);
+file($oops, 0);
+file_exists($oops);
+file_get_contents($oops, true);
+file_put_contents($oops, $data);
+fileatime($oops);
+filectime($oops);
+filegroup($oops);
+fileinode($oops);
+filemtime($oops);
+fileowner($oops);
+fileperms($oops);
+filesize($oops);
+filetype($oops);
+fopen($oops, "r");
+glob($oops . "/**", 0);
+is_dir($oops);
+is_executable($oops);
+is_file($oops);
+is_link($oops);
+is_readable($oops);
+is_uploaded_file($oops);
+is_writable($oops);
+is_writeable($oops);
+lchgrp($oops, 1);
+lchown($oops, 1);
+link($oops, $ok);
+link($ok, $oops);
+linkinfo($oops);
+lstat($oops);
+mkdir($oops, 0777);
+move_uploaded_file($ok, $oops);
+parse_ini_file($oops, true);
+pathinfo($oops, PATHINFO_DIRNAME);
+readfile($oops, true);
+readlink($oops);
+rename($oops, $ok, null);
+rename($ok, $oops, null);
+rmdir($oops, null);
+stat($oops);
+symlink($oops, $ok);
+symlink($ok, $oops);
+tempnam($oops, $ok);
+tempnam($ok, $oops);
+touch($oops, time());
+unlink($oops, null);
+
+// directory
+chdir($oops);
+chroot($oops);
+dir($oops, null);
+opendir($oops, null);
+scandir($oops, SCANDIR_SORT_ASCENDING);
---
+++ tests/php/lang/path_using_user_input/testdata/ok.php
@@ -0,0 +1,63 @@
+<?php
+
+// filesystem
+chgrp($ok, 1);
+chmod($ok, 0755);
+chown($ok, 1);
+copy($ok, $ok, null);
+copy($ok, $ok);
+disk_free_space($ok);
+diskfreespace($ok);
+disk_total_space($ok);
+file($ok, 0);
+file_exists($ok);
+file_get_contents($ok, true);
+file_put_contents($ok, $data);
+fileatime($ok);
+filectime($ok);
+filegroup($ok);
+fileinode($ok);
+filemtime($ok);
+fileowner($ok);
+fileperms($ok);
+filesize($ok);
+filetype($ok);
+fopen($ok, "r");
+glob($ok . "/**", 0);
+is_dir($ok);
+is_executable($ok);
+is_file($ok);
+is_link($ok);
+is_readable($ok);
+is_uploaded_file($ok);
+is_writable($ok);
+is_writeable($ok);
+lchgrp($ok, 1);
+lchown($ok, 1);
+link($ok, $ok);
+link($ok, $ok);
+linkinfo($ok);
+lstat($ok);
+mkdir($ok, 0777);
+move_uploaded_file($ok, $ok);
+parse_ini_file($ok, true);
+pathinfo($ok, PATHINFO_DIRNAME);
+readfile($ok, true);
+readlink($ok);
+rename($ok, $ok, null);
+rename($ok, $ok, null);
+rmdir($ok, null);
+stat($ok);
+symlink($ok, $ok);
+symlink($ok, $ok);
+tempnam($ok, $ok);
+tempnam($ok, $ok);
+touch($ok, time());
+unlink($ok, null);
+
+// directory
+chdir($ok);
+chroot($ok);
+dir($ok, null);
+opendir($ok, null);
+scandir($ok, SCANDIR_SORT_ASCENDING);
