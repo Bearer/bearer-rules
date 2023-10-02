326c1ccfa3cf87a7f5554b195543a9a0226ac0e7
David Roe <dave.roe@bearer.sh>
2023-10-02 12:07:09 +0100

    feat: add hardcoded secret rule

---
+++ tests/php/lang/hardcoded_secret/testdata/bad.php
@@ -0,0 +1,10 @@
+<?php
+
+$password = "oops";
+
+const API_KEY = "oops";
+
+foo([ "secret" => "oops", "other" => 42]);
+
+$name = "SECRET_PASSWORD";
+define($name, "oops");
---
+++ tests/php/lang/hardcoded_secret/testdata/ok.php
@@ -0,0 +1,12 @@
+<?php
+
+$password = $ok;
+
+const API_KEY = $ok;
+
+foo([ "secret" => $ok, "other" => 42]);
+
+$name = "SECRET_PASSWORD";
+define($name, $ok);
+define($name, "**");
+define($name, "•••••");
