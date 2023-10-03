ba5de872591596f96527fd47cfb724413afddc28
Cédric Fabianski <cfabianski@me.com>
2023-10-03 15:09:19 +0200

    feat(golang): enable golang

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -16,7 +16,10 @@ cp -R $test_location $tmp_location
 
 if [ -n "$BEARER_WORKSPACE" ]; then
   cd $BEARER_WORKSPACE
-  BEARER_PYTHON_ENABLED=true BEARER_PHP_ENABLED=true go run ./cmd/bearer/main.go scan $tmp_location \
+  BEARER_GOLANG_ENABLED=true \
+  BEARER_PYTHON_ENABLED=true \
+  BEARER_PHP_ENABLED=true \
+  go run ./cmd/bearer/main.go scan $tmp_location \
     --only-rule=$rule_id \
     --quiet \
     --disable-default-rules=true \
@@ -32,8 +35,9 @@ else
     --rm \
     -v /tmp/bearer-scan:/tmp/bearer-scan \
     -v $rule_loc:/tmp/rules \
-    -e BEARER_PYTHON_ENABLED=true \
     -e BEARER_PHP_ENABLED=true \
+    -e BEARER_PYTHON_ENABLED=true \
+    -e BEARER_GOLANG_ENABLED=true \
     bearer/bearer:$BEARER_VERSION \
       scan $tmp_location \
       --only-rule=$rule_id \
