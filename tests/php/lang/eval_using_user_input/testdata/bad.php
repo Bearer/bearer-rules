1e99e70468cc6363e9de6db4de2a59fafbfe60d2
David Roe <dave.roe@bearer.sh>
2023-10-09 12:01:23 +0100

    feat: add more cases to eval rule

--- tests/php/lang/eval_using_user_input/testdata/bad.php
+++ tests/php/lang/eval_using_user_input/testdata/bad.php
@@ -2,5 +2,8 @@
 
 eval("echo " . $_GET["oops"]);
 
-call_user_func("func_" . $_POST["oops"], 42)
-call_user_func_array("func_" . $_POST["oops"], [42])
+call_user_func("func_" . $_POST["oops"], 42);
+call_user_func_array("func_" . $_POST["oops"], [42]);
+
+assert($_POST["oops"], "oops");
+array_map($_POST["oops"], $array);
--- tests/php/lang/eval_using_user_input/testdata/ok.php
+++ tests/php/lang/eval_using_user_input/testdata/ok.php
@@ -4,3 +4,6 @@ eval("echo " . $ok);
 
 call_user_func("func_" . $ok, 42)
 call_user_func_array("func_" . $ok, [42])
+
+assert($ok, "ok");
+array_map($ok, $array);
