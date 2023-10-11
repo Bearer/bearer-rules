413ba3de2a81087038995a8e4590312735c74ff5
David Roe <dave.roe@bearer.sh>
2023-10-11 12:15:33 +0100

    fix: add request uri to user input

--- tests/php/lang/open_redirect/testdata/bad.php
+++ tests/php/lang/open_redirect/testdata/bad.php
@@ -1,3 +1,4 @@
 <?php
 
 header("Location: {$_GET['oops']}", true, 301);
+header("Location: {$_SERVER['REQUEST_URI']}", true, 301);
--- tests/php/lang/open_redirect/testdata/ok.php
+++ tests/php/lang/open_redirect/testdata/ok.php
@@ -2,3 +2,4 @@
 
 header("Location: $ok", true, 301);
 header("Location: /foo", true, 301);
+header("Location: {$_SERVER['OTHER']}", true, 301);
