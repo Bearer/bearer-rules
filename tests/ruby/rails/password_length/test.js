3069d19e58b84c752d2b2ebb5515b2990814be1f
Cédric Fabianski <cfabianski@me.com>
2023-10-18 10:06:43 +0200

    feat(ruby): improve devise line matching

--- tests/ruby/rails/password_length/test.js
+++ tests/ruby/rails/password_length/test.js
@@ -3,17 +3,19 @@ const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
 
 describe(ruleId, () => {
   const invoke = createInvoker(ruleId, ruleFile, testBase)
-  
 
   test("ok_password_length", () => {
     const testCase = "ok_password_length.rb"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
 
   test("password_too_short", () => {
     const testCase = "password_too_short.rb"
-    expect(invoke(testCase)).toMatchSnapshot();
+    expect(invoke(testCase)).toMatchSnapshot()
   })
-  
+
+  test("password_too_short_devise", () => {
+    const testCase = "devise.rb"
+    expect(invoke(testCase)).toMatchSnapshot()
+  })
 })
