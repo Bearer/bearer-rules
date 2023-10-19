4e290d8970c91ba475ee7675efda61c884edec75
David Roe <dave.roe@bearer.sh>
2023-10-19 13:26:22 +0100

    fix: add more cases to xpath injection rule

--- tests/php/lang/xpath_injection/testdata/bad.php
+++ tests/php/lang/xpath_injection/testdata/bad.php
@@ -8,3 +8,9 @@ $xpath->query($userInput, $contextNode);
 
 $element = new SimpleXMLElement("<x></x>");
 $element->xpath($userInput);
+$element2 = simplexml_import_dom($node);
+$element2->xpath($userInput);
+$element3 = simplexml_load_file($filename);
+$element3->xpath($userInput);
+$element4 = simplexml_load_string($data);
+$element4->xpath($userInput);
