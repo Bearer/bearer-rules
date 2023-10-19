9c30728f1ed7c2a2b1e7389f57e1a55e76971572
David Roe <dave.roe@bearer.sh>
2023-10-19 12:37:34 +0100

    fix: support more cases in third party logger rule

--- tests/php/third_parties/logger/testdata/ok.php
+++ tests/php/third_parties/logger/testdata/ok.php
@@ -4,3 +4,6 @@ $user = array('email' => 'user@email.com', 'uuid' => 'unique-identifier');
 $log->info('Adding a new user', $user->uuid());
 $log->info('Adding a new user', $user->uuid);
 $log->info('Adding a new user', $user["uuid"]);
+
+$this->logger->info('Adding a new user', $user->userId);
+$this->log->info('Adding a new user', $user->userId);
