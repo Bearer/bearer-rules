6ec9e3d6c186ac18728647952ab7c865e0403d30
Cédric Fabianski <cfabianski@me.com>
2023-11-07 14:46:47 +0100

    chore: add licensing for gosec

--- tests/go/gosec/injection/ssrf_injection/testdata/main.go
+++ tests/go/gosec/injection/ssrf_injection/testdata/main.go
@@ -1,7 +1,3 @@
-// License: Apache 2.0 (c) gosec
-// source: https://github.com/securego/gosec/blob/master/testutils/source.go
-// hash: bfb0f42
-
 // Input from the std in is considered insecure
 package main
 
--- tests/go/gosec/secrets/secrets/testdata/main.go
+++ tests/go/gosec/secrets/secrets/testdata/main.go
@@ -1,7 +1,3 @@
-// License: Apache 2.0 (c) gosec
-// source: https://github.com/securego/gosec/blob/master/testutils/source.go
-// hash: bfb0f42
-
 package main
 
 import "fmt"
