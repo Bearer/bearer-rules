2608d850e7ab46c3741531e0e020726cc7371d61
elsapet <elizabeth@bearer.sh>
2024-01-25 15:53:51 +0200

    feat(java): extend log injection rule

--- tests/java/lang/log_injection/test.js
+++ tests/java/lang/log_injection/test.js
@@ -1,13 +1,24 @@
-const { createInvoker, getEnvironment } = require("../../../helper.js")
+const { createInvoker, createNewInvoker, getEnvironment } = require("../../../helper.js")
 const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
 
 describe(ruleId, () => {
   const invoke = createInvoker(ruleId, ruleFile, testBase)
-  
 
   test("log_injection", () => {
     const testCase = "log_injection.java"
     expect(invoke(testCase)).toMatchSnapshot();
   })
-  
+
+  // new style tests
+  const invokeV2 = createNewInvoker(ruleId, ruleFile, testBase)
+
+  test("log_injection_v2", () => {
+    const testCase = "main.java"
+
+    const results = invokeV2(testCase)
+
+    expect(results.Missing).toEqual([])
+    expect(results.Extra).toEqual([])
+  })
+
 })
