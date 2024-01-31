035d70d31ea1e578d672a57bc909e1ebb2b099b3
Cédric Fabianski <cedric@bearer.sh>
2024-01-31 12:18:44 +0100

    chore: improve writing rules (#212)
    Co-authored-by: elsapet <elizabeth@bearer.sh>

---
+++ scripts/generate/ruleYAMLContent.js
@@ -0,0 +1,52 @@
+module.exports = function ruleYAMLContent(ruleID, language) {
+  return `imports:
+  - <shared_rule1>
+patterns:
+  - pattern: |
+      $<INIT>.$<METHOD>($<USER_INPUT>$<...>)
+    filters:
+      - variable: INIT
+        detection: ${ruleID}_init
+        scope: cursor
+      - variable: METHOD
+        values:
+          - method1
+          - method2
+      - variable: USER_INPUT
+        detection: shared_rule1
+        scope: result
+auxiliary:
+  - id: ${ruleID}_init
+    patterns:
+      - pattern1
+      - pattern: $<INIT>
+        filters:
+          - variable: INIT
+            detection: ${ruleID}_instance
+            scope: cursor
+  - id: ${ruleID}_instance
+    patterns:
+      - pattern2
+languages:
+  - ${language}
+metadata:
+  description: ""
+  remediation_message: |
+    ## Description
+
+    ## Remediations
+
+    ✅
+
+    ❌
+
+    ## References
+
+    - []()
+
+  cwe_id:
+    - CWE_ID
+  id: ${ruleID}
+  documentation_url: https://docs.bearer.com/reference/rules/${ruleID}
+`;
+};
---
+++ scripts/generate/testJSContent.js
@@ -0,0 +1,22 @@
+module.exports = function testJSContent(filePath, testCase) {
+  const testName = filePath.split('/').pop();
+
+  return `const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+  test("${testName}", () => {
+    const testCase = "${testCase}"
+
+    const results = invoke(testCase)
+
+    expect(results.Missing).toEqual([])
+    expect(results.Extra).toEqual([])
+  })
+})`;
+};
