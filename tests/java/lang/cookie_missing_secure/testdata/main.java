6e87c8308a0bf7feeda1b4e5a0ea7376e3bb25db
elsapet <elizabeth@bearer.sh>
2024-01-31 15:55:37 +0200

    feat(java): extend secure cookie rule

--- tests/java/lang/cookie_missing_secure/testdata/main.java
+++ tests/java/lang/cookie_missing_secure/testdata/main.java
@@ -1,3 +1,5 @@
+// Use bearer:expected java_lang_cookie_missing_secure to flag expected findings
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Cookie;
 
@@ -6,26 +8,35 @@ public class Test
   public static final String COOKIE_NAME = "someCookie";
 
    public void badCookie(HttpServletResponse res) {
-    // bearer:expected java_lang_cookie_missing_http_only
+    // bearer:expected java_lang_cookie_missing_secure
     javax.servlet.http.Cookie cookie = new javax.servlet.http.Cookie(COOKIE_NAME, "cookieValue");
-    cookie.setSecure(true);
+    cookie.setSecure(false);
     cookie.setMaxAge(60);
-    cookie.setHttpOnly(false);
+    cookie.setHttpOnly(true);
     res.addCookie(cookie);
   }
 
   public void badCookie2(HttpServletResponse res) {
-    // bearer:expected java_lang_cookie_missing_http_only
+    // bearer:expected java_lang_cookie_missing_secure
     javax.servlet.http.Cookie cookie = new javax.servlet.http.Cookie(COOKIE_NAME, "cookieValue");
-    cookie.setSecure(true);
+    cookie.setHttpOnly(true);
     cookie.setMaxAge(60);
     res.addCookie(cookie);
   }
 
   public void badJakartaCookie(HttpServletResponse response) {
-    // bearer:expected java_lang_cookie_missing_http_only
+    // bearer:expected java_lang_cookie_missing_secure
     jakarta.servlet.http.Cookie jakartaCookie = new jakarta.servlet.http.Cookie(COOKIE_NAME, "someCookieValue");
     jakartaCookie.setMaxAge(60);
     response.addCookie(jakartaCookie);
   }
+
+  public void badJakartaCookie2(HttpServletResponse response) {
+    // bearer:expected java_lang_cookie_missing_secure
+    jakarta.servlet.http.Cookie jakartaCookie = new jakarta.servlet.http.Cookie(COOKIE_NAME, "someCookieValue");
+    jakartaCookie.setSecure(false);
+    jakartaCookie.setMaxAge(60);
+    response.addCookie(jakartaCookie);
+  }
 }
+
