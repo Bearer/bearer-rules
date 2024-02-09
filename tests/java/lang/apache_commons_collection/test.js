d7e95d89e534f6e8d1fe928bac6d54490c77355c
elsapet <elizabeth@bearer.sh>
2024-02-09 17:08:49 +0200

    feat: add secure test

--- tests/java/lang/apache_commons_collection/test.js
+++ tests/java/lang/apache_commons_collection/test.js
@@ -7,7 +7,16 @@ const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
 describe(ruleId, () => {
   const invoke = createNewInvoker(ruleId, ruleFile, testBase)
 
-  test("apache_commons_collection", () => {
+  test("apache_commons_collection_secure", () => {
+    const testCase = "secure/"
+
+    const results = invoke(testCase)
+
+    expect(results.Missing).toEqual([])
+    expect(results.Extra).toEqual([])
+  })
+
+  test("apache_commons_collection_insecure", () => {
     const testCase = "insecure/"
 
     const results = invoke(testCase)
