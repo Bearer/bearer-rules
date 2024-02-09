001bb02f5636e9850739655e93a3f618b98f8553
David Roe <dave.roe@bearer.sh>
2024-02-09 14:26:23 +0000

    missing integrity check

--- tests/java/lang/missing_integrity_check/testdata/ok.java
+++ tests/java/lang/missing_integrity_check/testdata/ok.java
@@ -1,15 +1,17 @@
-public byte[] encryptMe(String text) throws GeneralSecurityException
-{
-  Cipher c = Cipher.getInstance("DES/GCM/PKCS5Padding");
+class Foo {
+  public byte[] encryptMe(String text) throws GeneralSecurityException
+  {
+    Cipher c = Cipher.getInstance("DES/GCM/PKCS5Padding");
 
-  c.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
-  return c.doFinal(text);
-}
+    c.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
+    return c.doFinal(text);
+  }
 
-public byte[] encryptMeAgain(String text) throws GeneralSecurityException
-{
-  Cipher c = Cipher.getInstance("RSA/CBC/PKCS5Padding");
+  public byte[] encryptMeAgain(String text) throws GeneralSecurityException
+  {
+    Cipher c = Cipher.getInstance("RSA/CBC/PKCS5Padding");
 
-  c.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
-  return c.doFinal(text);
+    c.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
+    return c.doFinal(text);
+  }
 }
