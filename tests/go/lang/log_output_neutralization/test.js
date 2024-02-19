6a471f21610c77738ac37fbe7cc609697b90d827
elsapet <elizabeth@bearer.sh>
2024-02-19 13:31:10 +0200

    feat(go): add log output neutralization (CWE-117) (#275)

---
+++ tests/go/lang/log_output_neutralization/test.js
@@ -0,0 +1,18 @@
+const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+  test("log_output_neutralization", () => {
+    const testCase = "main.go"
+
+    const results = invoke(testCase)
+
+    expect(results.Missing).toEqual([])
+    expect(results.Extra).toEqual([])
+  })
+})
---
+++ tests/go/lang/log_output_neutralization/testdata/main.go
@@ -0,0 +1,73 @@
+// Use bearer:expected go_lang_log_output_neutralization to flag expected findings
+
+package main
+
+import (
+	"bytes"
+	"os"
+
+	"log"
+
+	seelog "github.com/cihub/seelog"
+	glog "github.com/golang/glog"
+	zerolog "github.com/rs/zerolog"
+	logrus "github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+
+	"strconv"
+)
+
+func bad() {
+	var buf bytes.Buffer
+	logger := log.New(&buf, "logger: ", log.Lshortfile)
+
+	// bearer:expected go_lang_log_output_neutralization
+	logger.Print(os.Args[0])
+}
+
+func bad2() {
+	// bearer:expected go_lang_log_output_neutralization
+	zerolog.Info.Msg(os.Args[0])
+	// bearer:expected go_lang_log_output_neutralization
+	zerolog.Print(os.Args[0])
+}
+
+func bad3() {
+	// bearer:expected go_lang_log_output_neutralization
+	logrus.WithFields().Info(os.Args[0])
+
+	log := logrus.New()
+	// bearer:expected go_lang_log_output_neutralization
+	log.Error(os.Args[0])
+}
+
+func bad4() {
+	var cfg zap.Config
+	logger := zap.Must(cfg.Build())
+	defer logger.Sync()
+
+	// bearer:expected go_lang_log_output_neutralization
+	logger.Info(os.Args[0])
+	logger.Printf("Args: %s", os.Args[0])
+}
+
+func bad5() {
+	// bearer:expected go_lang_log_output_neutralization
+	seelog.Trace(os.Args[0])
+}
+
+func bad6() {
+	// bearer:expected go_lang_log_output_neutralization
+	glog.Fatalf(os.Args[0])
+}
+
+func ok(input string) {
+	var buf bytes.Buffer
+	logger := log.New(&buf, "logger: ", log.Lshortfile)
+	sanitizedInput := strconv.Quote(input)
+
+	logger.Print(sanitizedInput)
+	logger.Printf("Args: %q", os.Args[0])
+
+	zerolog.Info.Msg(sanitizedInput)
+}
