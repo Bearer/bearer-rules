dee04cc1a6e194bc98760722f6f3de1b7216d89f
elsapet <elizabeth@bearer.sh>
2024-02-20 15:53:35 +0200

    fix: golang injection rules

--- tests/go/gosec/subproc/subproc/testdata/main.go
+++ tests/go/gosec/subproc/subproc/testdata/main.go
@@ -1,133 +1,62 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"syscall"
+	"net/rpc"
+	"reflect"
 )
 
-func foo1() {
-	err := exec.CommandContext(context.Background(), "git", "rev-parse", "--show-toplavel").Run()
+type Args struct {
+	Input string
+}
+
+func bad(maliciousInput string) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
 	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Command finished with error: %v", err)
-}
-
-func foo2() {
-	err := exec.CommandContext(context.Background(), os.Args[0], "5").Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Command finished with error: %v", err)
-}
-
-func foo3() {
-	run := "sleep" + os.Getenv("SOMETHING")
-	cmd := exec.Command(run, "5")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-	log.Printf("Command finished with error: %v", err)
-}
-
-func foo4(command string) {
-	cmd := exec.Command(command, "5")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-}
-
-func foo5() {
-	foo4("sleep")
-}
-
-func foo6(a string, c string) {
-	cmd := exec.Command(c)
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-	cmd = exec.Command(a)
-	err = cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-}
-
-func foo7() {
-	foo6("ll", "ls")
-}
-
-func foo8() {
-	err := syscall.Exec("/bin/cat", []string{"/etc/passwd"}, nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	}
-}
-
-func foo9(command string) {
-	_, err := syscall.ForkExec(command, []string{}, nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	}
-}
-
-func foo10() {
-	foo9("sleep")
-}
-
-func foo11(command string) {
-	_, _, err := syscall.StartProcess(command, []string{}, nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	}
-}
-
-func foo12() {
-	foo11("sleep")
-}
-
-func foo13() {
-	run := "sleep"
-	cmd := exec.Command(run, "5")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-	log.Printf("Command finished with error: %v", err)
-}
-
-func foo14() {
-	err := exec.CommandContext(context.Background(), os.Args[0], "5").Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Command finished with error: %v", err)
-}
-
-func foo15(arg string) (results string, err error) {
-	commandLine := "mysql -h mysql -u root -prootwolf -e 'select adminsid from vulnapp.adminsessions where adminsessionid=\"" + arg + "\";'"
-
-	res, err := exec.Command("sh", "-c", commandLine).Output()
-	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Failed to connect to RPC server:", err)
+		return
 	}
 
-	return string(res), nil
+	var reply string
+	// bearer:expected go_gosec_subproc_subproc
+	err = client.Call("MyService.DoSomething", Args{Input: maliciousInput}, &reply)
+	if err != nil {
+		fmt.Println("RPC call failed:", err)
+		return
+	}
+
+	fmt.Println("Server response:", reply)
+}
+
+type User struct {
+	Username string
+	Role     string
+}
+
+func bad2(maliciousInput string) {
+	user := User{Username: "admin", Role: "user"}
+
+	// bearer:expected go_gosec_subproc_subproc
+	_ = reflect.ValueOf(user).FieldByName(maliciousInput)
+
+	// ...
+}
+
+func good(maliciousInput string) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		fmt.Println("Failed to connect to RPC server:", err)
+		return
+	}
+
+	var reply string
+	if maliciousInput == "delete" {
+		err = client.Call("MyService.DoSomething", Args{Input: "delete"}, &reply)
+	}
+	if err != nil {
+		fmt.Println("RPC call failed:", err)
+		return
+	}
+
+	fmt.Println("Server response:", reply)
 }
