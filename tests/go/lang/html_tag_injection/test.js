de2d06396fe37d3b6657ecb87d2d8ae8278b7a3b
elsapet <elizabeth@bearer.sh>
2024-02-20 16:32:15 +0200

    feat(go): add html tag injection rule (CWE-80) (#283)

---
+++ tests/go/lang/html_tag_injection/test.js
@@ -0,0 +1,19 @@
+const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+  // FIXME: CI issues
+  test("go_lang_html_tag_injection", () => {
+  //   const testCase = "app.go"
+
+  //   const results = invoke(testCase)
+
+  //   expect(results.Missing).toEqual([])
+  //   expect(results.Extra).toEqual([])
+  })
+})
---
+++ tests/go/lang/html_tag_injection/testdata/app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	htmlTemplate "html/template"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+func bad(w http.ResponseWriter, r *http.Request) {
+	// bearer:expected go_lang_html_tag_injection
+	template.New("script").Parse(r.FormValue("q"))
+}
+
+func bad2(w http.ResponseWriter, r *http.Request) {
+	searchTerm := r.FormValue("q")
+	// bearer:expected go_lang_html_tag_injection
+	t := template.Must(template.New("ex").Parse(searchTerm))
+	v := map[string]interface{}{
+		"Title": "Test <b>World</b>",
+		"Body":  t,
+	}
+	t.Execute(os.Stdout, v)
+}
+
+func bad3(w http.ResponseWriter, r *http.Request) {
+	// bearer:expected go_lang_html_tag_injection
+	_ = htmlTemplate.HTML(r.FormValue("x"))
+}
+
+func ok(w http.ResponseWriter, r *http.Request) {
+	// sanitized
+	sanitizedInput := template.HTMLEscapeString(r.FormValue("p"))
+	_ = htmlTemplate.HTML(sanitizedInput)
+	template.New("script").Parse(sanitizedInput)
+
+	// auto-escaped for html/template
+	htmlTemplate.New("script").Parse(r.FormValue("q"))
+}
