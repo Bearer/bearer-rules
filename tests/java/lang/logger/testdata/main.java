c5c1cd8d6aff98e5887a384ced78c9532a314113
elsapet <elizabeth@bearer.sh>
2024-02-21 18:10:39 +0200

    fix: use string literal

--- tests/java/lang/logger/testdata/main.java
+++ tests/java/lang/logger/testdata/main.java
@@ -1,15 +1,16 @@
 public class Foo
 {
-    public void bad()
+    public void bad(String someVar)
     {
         User user;
         // bearer:expected java_lang_logger
-        logger.error(user.name);
+        logger.error(someVar);
         // bearer:expected java_lang_logger
-        logger.error("This is not okay " + user.name);
+        logger.error("This is not okay " + someVar);
     }
 
-    public void ok() {
+    public void ok(User user) {
         logger.error("This is okay");
+        logger.error("This is bad but handled by another rule: " + user.name);
     }
 }
