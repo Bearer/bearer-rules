03b1d37c64d809197d96a2ac19a573f1be2d4347
gotbadger <p.j.h@hey.com>
2024-02-21 17:48:55 +0000

    fix: workaround issue in example

--- tests/ruby/third_parties/airbrake/testdata/datatype_in_rescue_block.rb
+++ tests/ruby/third_parties/airbrake/testdata/datatype_in_rescue_block.rb
@@ -1,6 +1,7 @@
 begin
   1/0
 rescue ZeroDivisionError => ex
-# bearer:expected ruby_third_parties_airbrake
+  1+1
+  # bearer:expected ruby_third_parties_airbrake
   response = Airbrake.notify_sync(current_user.first_name)
 end
