6369511cd54f22a68635d28540c45dcf49e90706
gotbadger <p.j.h@hey.com>
2024-02-22 10:48:46 +0000

    ci: add workaround for expected results

--- tests/ruby/third_parties/open_telemetry/testdata/datatype_in_record_exception.rb
+++ tests/ruby/third_parties/open_telemetry/testdata/datatype_in_record_exception.rb
@@ -4,7 +4,8 @@ current_span = OpenTelemetry::Trace.current_span
 begin
   1/0 # something that obviously fails
 rescue
-# bearer:expected ruby_third_parties_open_telemetry
+  1+1
+  # bearer:expected ruby_third_parties_open_telemetry
   current_span.status = OpenTelemetry::Trace::Status.error("error for user #{current_user.email}")
 end
 
@@ -15,6 +16,6 @@ begin
 rescue Exception => ex
   current_span.status = OpenTelemetry::Trace::Status.error("error message here!")
   current_span.record_exception(ex)
-# bearer:expected ruby_third_parties_open_telemetry
+  # bearer:expected ruby_third_parties_open_telemetry
   current_span.record_exception(ex, attributes: { "user.ip" => user.ip_address })
 end
