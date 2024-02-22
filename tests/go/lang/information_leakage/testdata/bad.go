012b0cf00c2a8fe3da9694304f4e18d186ceb8ec
Philip Hayton <gotbadger@users.noreply.github.com>
2024-02-22 11:58:24 +0000

    ci(go): update to use annotations (#303)

--- tests/go/lang/information_leakage/testdata/bad.go
+++ tests/go/lang/information_leakage/testdata/bad.go
@@ -6,6 +6,7 @@ func fetchUser(w http.ResponseWriter, r *http.Request) {
 	_, err := getUserFromDB(r.FormValue("username"))
 	if err != nil {
 		// Exposing detailed error to the client
+// bearer:expected go_lang_information_leakage
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	// ...
--- tests/go/lang/weak_hash_sha1/testdata/bad.go
+++ tests/go/lang/weak_hash_sha1/testdata/bad.go
@@ -12,8 +12,10 @@ func main() {
 	user := User{
 		DateOfBirth: "01/01/1970"
 	}
+// bearer:expected go_lang_weak_hash_sha1
 	fmt.Printf("%x", h.Sum(user.DateOfBirth))
 
 	data := []byte(user.DateOfBirth)
+// bearer:expected go_lang_weak_hash_sha1
 	fmt.Printf("%x", sha1.Sum(data))
 }
