fcf47f72495ace26ef7947e00262f2edb4cb0e94
Cédric Fabianski <cfabianski@me.com>
2024-02-22 11:41:56 +0100

    test: update examples

--- tests/javascript/lang/logger_leak/testdata/app.js
+++ tests/javascript/lang/logger_leak/testdata/app.js
@@ -5,6 +5,8 @@ const ctx = {
   },
 }
 
+const knownData = "foo"
+
 logger.child(ctx).info(user.name)
 logger.child(ctx)
 console.log(user.name)
@@ -13,9 +15,15 @@ console.log("user email is", user.email)
 console.log(user.name ? ok : ok)
 logger.info(user.email)
 logger.error("something went wrong")
+// bearer:expected javascript_lang_logger_leak
+logger.error("something went wrong with unknown data" + unknownData)
+logger.error("something went wrong with known data" + knownData)
+
+// bearer:expected javascript_lang_logger_leak
+console.log("something went wrong", unknownData)
 
 const user = { email: "foo@example.com" }
 
-const data = getResults()
+const unknownResults = getResults()
 // bearer:expected javascript_lang_logger_leak
-logger.info(`this is a leak ${data}`)
+logger.info(`this is a leak ${unknownResults}`)
