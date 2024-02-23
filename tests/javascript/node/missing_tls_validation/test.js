ed11578d7cd012e1bafcbf4a72c9c3119ea6eeaa
elsapet <elizabeth@bearer.sh>
2024-02-23 18:48:35 +0200

    feat(js): add missing TLS rule for Node (#316)

---
+++ tests/javascript/node/missing_tls_validation/test.js
@@ -0,0 +1,20 @@
+const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+  test("missing_tls_validation", () => {
+    const testCase = "app.js"
+
+    const results = invoke(testCase)
+
+    expect(results).toEqual({
+      Missing: [],
+      Extra: []
+    })
+  })
+})
