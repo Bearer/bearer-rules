9b6c67f79ca3ed45e4bd0feb575a85c2c4464c89
David Roe <dave.roe@bearer.sh>
2024-03-12 16:39:47 +0000

    feat: bigquery rule

---
+++ tests/go/third_parties/bigquery/testdata/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/bigquery"
+)
+
+func insertRows(projectID, datasetID, tableID string) error {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	inserter := client.Dataset(datasetID).Table(tableID).Inserter()
+
+	// bearer:expected go_third_parties_bigquery
+	if err := inserter.Put(ctx, []*Item{{Email: user.email}}); err != nil {
+		return err
+	}
+	if err := inserter.Put(ctx, []*Item{{Ok: user.count}}); err != nil {
+		return err
+	}
+
+	return nil
+}
