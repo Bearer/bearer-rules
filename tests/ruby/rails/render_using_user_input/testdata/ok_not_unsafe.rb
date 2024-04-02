0a40950e119d52441c791be04a7b9e66e94c3093
David Roe <dave.roe@bearer.sh>
2024-04-02 15:28:40 +0100

    fix(ruby): only consider inline disposition as xss for send_data (#358)

--- tests/ruby/rails/render_using_user_input/testdata/ok_not_unsafe.rb
+++ tests/ruby/rails/render_using_user_input/testdata/ok_not_unsafe.rb
@@ -4,4 +4,5 @@ render inline: "ok"
 render html: sanitize(params[:oops])
 render inline: "<h1>#{strip_tags(params[:oops])}</h1>"
 
-send_data "ok", type: content_type
+send_data params[:user_input], type: content_type
+send_data params[:user_input], type: content_type, disposition: "attachment"
--- tests/ruby/rails/render_using_user_input/testdata/unsafe.rb
+++ tests/ruby/rails/render_using_user_input/testdata/unsafe.rb
@@ -4,4 +4,6 @@ render html: params[:oops]
 render inline: "<h1>#{params[:oops]}</h1>"
 
 # bearer:expected ruby_rails_render_using_user_input
-send_data params[:oops], type: content_type
+send_data params[:user_input], type: content_type, disposition: "inline"
+# bearer:expected ruby_rails_render_using_user_input
+send_data params[:user_input], type: content_type, disposition: unknown
