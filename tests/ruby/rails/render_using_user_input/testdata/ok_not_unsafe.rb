e001462b5fbf32eb237c644e5c0801484ef928c1
David Roe <didroe@gmail.com>
2024-04-02 12:54:12 +0100

    fix(ruby): add missing rails XSS send_data case (#356)

--- tests/ruby/rails/render_using_user_input/testdata/ok_not_unsafe.rb
+++ tests/ruby/rails/render_using_user_input/testdata/ok_not_unsafe.rb
@@ -3,3 +3,5 @@ render inline: "ok"
 
 render html: sanitize(params[:oops])
 render inline: "<h1>#{strip_tags(params[:oops])}</h1>"
+
+send_data "ok", type: content_type
--- tests/ruby/rails/render_using_user_input/testdata/unsafe.rb
+++ tests/ruby/rails/render_using_user_input/testdata/unsafe.rb
@@ -2,3 +2,6 @@
 render html: params[:oops]
 # bearer:expected ruby_rails_render_using_user_input
 render inline: "<h1>#{params[:oops]}</h1>"
+
+# bearer:expected ruby_rails_render_using_user_input
+send_data params[:oops], type: content_type
