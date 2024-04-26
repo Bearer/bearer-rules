961bb139ca2852f78c2bb6a792b53ab128526c2f
elsapet <elizabeth@bearer.sh>
2024-04-26 11:59:04 +0200

    feat: use external input in sqli rules

--- tests/java/spring/sqli/testdata/bad.java
+++ tests/java/spring/sqli/testdata/bad.java
@@ -11,14 +11,22 @@ public class Foo {
    public void updateUser() {
       String myQueryStr = "update user set name='"+request.getParameter("name")+"' where id='"+request.getParameter("userId")+"'";
 
-// bearer:expected java_spring_sqli
+      // bearer:expected java_spring_sqli
       var factory = new PreparedStatementCreatorFactory(myQueryStr, Types.VARCHAR);
-// bearer:expected java_spring_sqli
+      // bearer:expected java_spring_sqli
       factory.newPreparedStatementCreator(myQueryStr, []);
 
-// bearer:expected java_spring_sqli
+      // bearer:expected java_spring_sqli
       t.batchUpdate("ok", myQueryStr);
-// bearer:expected java_spring_sqli
+      // bearer:expected java_spring_sqli
       t.execute(myQueryStr);
+   }
+
+   public void dynamicVarTest(String someString, String[] args) {
+      String myQueryStr = "select " + someString;
+      // bearer:expected java_spring_sqli
+      t.execute(myQueryStr);
+      // bearer:expected java_spring_sqli
+      t.execute("select " + args[1]);
    }
 }
