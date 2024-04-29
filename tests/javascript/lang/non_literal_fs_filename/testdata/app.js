68ac136db10c18705afac85b911e38e5562776d5
elsapet <elizabeth@bearer.sh>
2024-04-29 10:17:59 +0200

    fix(js): non-literal filename rule

--- tests/javascript/lang/non_literal_fs_filename/testdata/app.js
+++ tests/javascript/lang/non_literal_fs_filename/testdata/app.js
@@ -11,6 +11,25 @@ export function bad(options) {
   }
 }
 
+export function bad2(options) {
+  // bearer:expected javascript_lang_non_literal_fs_filename
+  Fs.copyFileSync(options.filePath, 'some-new-filepath.txt', 'utf8');
+
+  // bearer:expected javascript_lang_non_literal_fs_filename
+  Fs.symlink('some-filepath.txt', options.filePath, 'utf8');
+
+  // bearer:expected javascript_lang_non_literal_fs_filename
+  Fs.rename(options.filePath, options.newFilePath);
+}
+
 export function ok() {
   stdioTarget = Fs.createWriteStream('some-string-literal', 'utf8');
+}
+
+export function ok2(data, encoding) {
+  stdioTarget = Fs.createWriteStream('some-string-literal', data, encoding);
+}
+
+export function ok3(data) {
+  Fs.symlink('some-filepath.txt', 'some-other-filepath.txt', data.options);
 }
