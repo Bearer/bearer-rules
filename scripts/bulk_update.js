9e2ee69ffc3953112a4cff782eacae4f6dc6d3f8
elsapet <elizabeth@bearer.sh>
2024-04-30 10:27:06 +0200

    feat: update rule remediation messages (#379)

--- scripts/bulk_update.js
+++ scripts/bulk_update.js
@@ -4,7 +4,6 @@ const { readFile, readdir, writeFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
 const { parse } = require("csv-parse/sync");
 
-
 const program = new Command();
 
 async function main() {
@@ -18,7 +17,7 @@ async function main() {
       ',',
     )
     .action(async function(){
-      dataSet = await load()
+      const dataSet = await load()
       await Promise.all(dataSet.map(updateRule))
     });
 
@@ -27,30 +26,37 @@ async function main() {
 
 
 async function load() {
-  const data = {}
   const filePath = resolve(program.args[0]);
   const contents = await readFile(filePath, { encoding: 'utf8' });
   return parse(contents, { delimiter: program.opts().csvDelimiter, from_line: 2 })
 }
 
 async function updateRule(row) {
-  const [lang, rule_description, rule_id, _framework, cwe, _doc] = row
-  const filePath = await findRuleFile(rule_id, lang)
+  const [lang, ruleDescription, ruleId, _framework, cwe, _doc, remediationMessage] = row
+  const filePath = await findRuleFile(ruleId, lang)
+
   let save = false
   const ruleContents = await readFile(filePath, { encoding: 'utf8' })
   const doc = YAML.parseDocument(ruleContents)
 
   // update desc node
-  const desc_path = ["metadata", "description"]
-  if(doc.contents.getIn(desc_path) != rule_description){
-    doc.contents.setIn(desc_path, rule_description)
+  const descPath = ["metadata", "description"]
+  if(doc.contents.getIn(descPath) != ruleDescription){
+    doc.contents.setIn(descPath, ruleDescription)
     save = true
   }
 
   //udpate cwe node?
-  const cwe_path = ["metadata", "cwe_id"]
-  if(doc.contents.getIn(cwe_path).toJS(doc) != [parseInt(cwe)]){
-    doc.contents.setIn(cwe_path, [parseInt(cwe)])
+  const cwePath = ["metadata", "cwe_id"]
+  if(doc.contents.getIn(cwePath).toJS(doc) != [parseInt(cwe)]){
+    doc.contents.setIn(cwePath, [parseInt(cwe)])
+    save = true
+  }
+
+  // update remediation message
+  const remediationPath = ["metadata", "remediation_message"]
+  if(doc.contents.getIn(remediationPath) != remediationMessage){
+    doc.contents.setIn(remediationPath, remediationMessage)
     save = true
   }
 
@@ -60,29 +66,30 @@ async function updateRule(row) {
 }
 
 async function findRuleFile(id, lang) {
-  let ruleRemenant = id.substring(lang.length+1)+".yml"
+  let ruleRemnant = id.substring(lang.length+1)+".yml"
   let path = "./rules/"+lang+"/"
-  while(ruleRemenant != null){
-    [path, ruleRemenant] = await findInPath(path, ruleRemenant)
+  while(ruleRemnant != null){
+    [path, ruleRemnant] = await findInPath(path, ruleRemnant)
   }
   return path
 }
 
-async function findInPath(path, ruleRemenant){
+async function findInPath(path, ruleRemnant){
   let dirs = await readdir(path)
 
   for(let dir of dirs) {
-    if(ruleRemenant == dir){
-      return [path+ruleRemenant, null]
+    console.log("dir "+dir)
+    if(ruleRemnant == dir){
+      return [path+ruleRemnant, null]
     }
-    if(ruleRemenant.startsWith(dir)){
-      ruleRemenant = ruleRemenant.substring(dir.length+1)
+    if(ruleRemnant.startsWith(dir)){
+      ruleRemnant = ruleRemnant.substring(dir.length+1)
       path = path + dir + "/"
-      return [path, ruleRemenant]
+      return [path, ruleRemnant]
     }
   }
 
-  throw new Error(path+":"+ruleRemenant+" did not match any rule file")
+  throw new Error(path+ruleRemnant+" did not match any rule file")
 
 }
 
