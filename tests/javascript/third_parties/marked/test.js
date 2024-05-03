3fb7b8aeb042fd575fe98236f03d5a91935d9653
elsapet <elizabeth@bearer.sh>
2024-05-03 09:33:35 +0200

    feat(js): marked dependency vulnerability (#384)

---
+++ tests/javascript/third_parties/marked/test.js
@@ -0,0 +1,39 @@
+const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+    // FIXME: investigate - CI failure!
+    // passes individually, fails when run together with
+    // dom_purify test case
+    // test("insecure", () => {
+    //   const testCase = "insecure"
+
+    //   const results = invoke(testCase)
+
+    //   expect(results.Missing).toEqual([])
+    //   expect(results.Extra).toEqual([])
+    // })
+
+    test("not_used", () => {
+      const testCase = "not_used"
+
+      const results = invoke(testCase)
+
+      expect(results.Missing).toEqual([])
+      expect(results.Extra).toEqual([])
+    })
+
+    test("secure", () => {
+      const testCase = "secure"
+
+      const results = invoke(testCase)
+
+      expect(results.Missing).toEqual([])
+      expect(results.Extra).toEqual([])
+    })
+})
