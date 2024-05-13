8c64e6c52525572e52e5a09413abcc4f120b0ccd
elsapet <elizabeth@bearer.sh>
2024-05-13 11:56:12 +0200

    fix: extend pattern to include raise from syntax

--- tests/python/lang/exception/testdata/main.py
+++ tests/python/lang/exception/testdata/main.py
@@ -4,6 +4,13 @@ def bad(user):
     raise CustomError(f"User '{user.email}' is unauthorized")
     # bearer:expected python_lang_exception
     raise Exception(f"User '{user.email}' is unauthorized")
+    
+def bad2(user):
+  try: 
+    user.save()
+  except Exception as e:
+    # bearer:expected python_lang_exception
+    raise BadUserException(f"User can't be saved: " + user.email) from e
 
 def ok(user)
   if not user.logged_in:
