cfa8c11db3dbda28eed0303a1617ebc151cea4e1
elsapet <elizabeth@bearer.sh>
2024-05-15 15:55:44 +0200

    feat(python): code injection (CWE-94) (#397)

---
+++ tests/python/lang/code_injection/testdata/main.py
@@ -0,0 +1,16 @@
+# Use bearer:expected python_lang_code_injection to flag expected findings
+def bad(request):
+  form = BadForm(request.POST)
+  # bearer:expected python_lang_code_injection
+  exec(form.cleaned_data["some_code"])
+  
+def bad2():
+  username = input("what hack today?")
+  # bearer:expected python_lang_code_injection
+  setattr(current_user, "name", username)
+
+import os
+def bad3(request):
+  unsafe = request.GET.get("some_code")
+  # bearer:expected python_lang_code_injection
+  os.execl("/bin/bash", "/bin/bash", "-c", unsafe)
