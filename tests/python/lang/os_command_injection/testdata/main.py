31e12b25974873827e35c0d922079df35eea9f5d
elsapet <elizabeth@bearer.sh>
2024-05-16 11:44:21 +0200

    feat(python): os command injection (CWE-78) (#398)

---
+++ tests/python/lang/os_command_injection/testdata/main.py
@@ -0,0 +1,31 @@
+# Use bearer:expected python_lang_os_command_injection to flag expected findings
+
+my_os = __import__("os")
+# bearer:expected python_lang_os_command_injection
+getattr(my_os, "system")(input())
+
+import os
+import subprocess as subproc
+def bad():
+  unsafe = input("what hack today?")
+  # bearer:expected python_lang_os_command_injection
+  subproc.run([unsafe, "exit 0"], shell=True, capture_output=True)
+  # bearer:expected python_lang_os_command_injection
+  os.system(unsafe)
+  
+  # bearer:expected python_lang_os_command_injection
+  subproc.check_output(
+    unsafe,
+    stderr=subprocess.STDOUT,
+    shell=True)
+
+def bad2():
+  unsafe = sys.argv[1]
+  # bearer:expected python_lang_os_command_injection
+  os.spawnlp(os.P_WAIT, unsafe)
+  # bearer:expected python_lang_os_command_injection
+  os.spawnve(os.P_WAIT, "/bin/bash", ["-c", unsafe], os.environ)
+ 
+def ok():
+  os.spawnve(os.P_WAIT, "/bin/ls", ["-a"], os.environ)
+  subproc.run(["dir"], shell=False)
