b2bea828e3c2907c08a901efdcb30cf36b206fd3
elsapet <elizabeth@bearer.sh>
2024-05-16 13:33:40 +0200

    fix: use instance case

--- tests/python/django/insecure_cookie/testdata/main.py
+++ tests/python/django/insecure_cookie/testdata/main.py
@@ -1,8 +1,13 @@
+from django.http import HttpResponse
+
 def bad():
+  response = HttpResponse()
   # bearer:expected python_django_insecure_cookie
-  HttpResponse.set_cookie("foo", "bar", max_age=None, secure=False, httponly=False)
+  response.set_cookie("foo", "bar", max_age=None, secure=False, httponly=False)
   
 def ok(): 
-  HttpResponse.set_cookie("foo", "bar")
+  response = HttpResponse()
+  
+  response.set_cookie("foo", "bar")
   # still bad but not for this rule
-  HttpResponse.set_cookie("foo", "bar", max_age=None, secure=True, httponly=False)
+  response.set_cookie("foo", "bar", max_age=None, secure=True, httponly=False)
