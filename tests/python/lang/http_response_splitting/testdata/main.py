ef0a7f6919e12be648e3e51f773d253a819a3630
David Roe <dave.roe@bearer.sh>
2024-05-16 10:31:27 +0100

    feat: add http response splitting rule

---
+++ tests/python/lang/http_response_splitting/testdata/main.py
@@ -0,0 +1,32 @@
+# this file is also testing the shared instance rule
+
+from http.server import BaseHTTPRequestHandler
+
+class WebRequestHandler(BaseHTTPRequestHandler, Foo):
+    def do_GET(self):
+        self.send_header("ok", "ok")
+        
+        # bearer:expected python_lang_http_response_splitting
+        self.send_header(self.path, "ok")
+
+    def do_POST(self):
+        self.send_header(self.path.replace("\r\n", ""), "ok")
+        self.send_header("ok", self.path.replace("\r\n", ""))
+        
+        # bearer:expected python_lang_http_response_splitting
+        self.send_header("ok", self.path)
+
+
+# contrived examples for testing instance rule
+class Other:
+    def m(self, x: BaseHTTPRequestHandler) -> string:
+        # bearer:expected python_lang_http_response_splitting
+        x.send_header(input(), "ok")
+
+        y = BaseHTTPRequestHandler(foo)
+        # bearer:expected python_lang_http_response_splitting
+        y.send_header(input(), "ok")
+
+    def m(self, z: BaseHTTPRequestHandler = default) -> string:
+        # bearer:expected python_lang_http_response_splitting
+        z.send_header(input(), "ok")
