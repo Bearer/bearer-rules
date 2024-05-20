cf8e2d25170adc79c018a25205971173af77e8a6
elsapet <elizabeth@bearer.sh>
2024-05-20 11:28:07 +0200

    fix: extend pattern and add tests

--- tests/python/django/debug_mode_enabled/testdata/main.py
+++ tests/python/django/debug_mode_enabled/testdata/main.py
@@ -1,1 +1,13 @@
-# Use bearer:expected python_django_debug_mode_enabled to flag expected findings
+from django.conf import settings
+
+# bearer:expected python_django_debug_mode_enabled
+DEBUG = True
+
+def bad():
+  # bearer:expected python_django_debug_mode_enabled
+  settings.configure(DEBUG=True)
+
+DEBUG = False
+
+def ok():
+  settings.configure(DEBUG=False)
