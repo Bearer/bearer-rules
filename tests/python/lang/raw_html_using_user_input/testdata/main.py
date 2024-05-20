1fb63b8c6ae12e9be464ba9a85deb4da794f726d
David Roe <dave.roe@bearer.sh>
2024-05-20 10:52:07 +0100

    feat: raw html using user input rule

---
+++ tests/python/lang/raw_html_using_user_input/testdata/main.py
@@ -0,0 +1,13 @@
+from html_sanitizer import Sanitizer
+
+user_input = input()
+
+def bad():
+    # bearer:expected python_lang_raw_html_using_user_input
+    html = f"<strong>{user_input}</strong>"
+
+def ok():
+    sanitizer = Sanitizer()
+    sanitized_value = sanitizer.sanitize(user_input)
+
+    html = f"<strong>{sanitized_value}</strong>"
