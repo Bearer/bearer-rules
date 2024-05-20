71ee8d1a5f3708961a6e0de78ac3483570a8a74a
elsapet <elizabeth@bearer.sh>
2024-05-20 11:15:16 +0200

    feat(python): bind to all interfaces (CWE-200) (#408)

---
+++ tests/python/lang/bind_to_all_interfaces/test.js
@@ -0,0 +1,20 @@
+const {
+  createNewInvoker,
+  getEnvironment,
+} = require("../../../helper.js")
+const { ruleId, ruleFile, testBase } = getEnvironment(__dirname)
+
+describe(ruleId, () => {
+  const invoke = createNewInvoker(ruleId, ruleFile, testBase)
+
+  test("bind_to_all_interfaces", () => {
+    const testCase = "main.py"
+
+    const results = invoke(testCase)
+
+    expect(results).toEqual({
+      Missing: [],
+      Extra: []
+    })
+  })
+})
