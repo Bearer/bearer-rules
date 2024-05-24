fc75a0c67a6357ce91834af8762d5f56f51d7796
elsapet <elizabeth@bearer.sh>
2024-05-24 16:15:48 +0200

    fix(python/django): FileSystemStorage call

--- tests/python/django/path_using_user_input/testdata/main.py
+++ tests/python/django/path_using_user_input/testdata/main.py
@@ -1,10 +1,10 @@
 from django.core.files.storage import FileSystemStorage as FSS
 
 # bearer:expected python_django_path_using_user_input
-fs = FSS(form.cleaned_data["storage_path"])
+fs = FSS(location=form.cleaned_data["storage_path"])
 request_file = request.FILES['document']
 file = fs.save(request_file)
 
 from django.core.files.storage import default_storage
 # bearer:expected python_django_path_using_user_input
-default_storage.save(form.cleaned_data["filepath"])
+default_storage.save(location=form.cleaned_data["filepath"])
