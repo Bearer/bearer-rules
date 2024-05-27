5c623966d29070e27bbc123dfeec019de5d3334f
David Roe <dave.roe@bearer.sh>
2024-05-27 16:36:13 +0100

    feat(python): add cwe-326 inadequate encryption strength rules

---
+++ tests/python/django/weak_secret_key/testdata/main.py
@@ -0,0 +1,7 @@
+SECRET_KEY = "correct-horse-battery-staple"
+# bearer:expected python_django_weak_secret_key
+SECRET_KEY = "weak"
+
+CRYPTOGRAPHY_KEY = "correct-horse-battery-staple"
+# bearer:expected python_django_weak_secret_key
+CRYPTOGRAPHY_KEY = "weak"
