6cc1c590315c03359a6970aa70ab7d0ee04b0d4b
elsapet <elizabeth@bearer.sh>
2024-05-27 12:10:42 +0200

    fix: add patterns for positional arguments

--- tests/python/django/path_using_user_input/testdata/main.py
+++ tests/python/django/path_using_user_input/testdata/main.py
@@ -5,6 +5,12 @@ fs = FSS(location=form.cleaned_data["storage_path"])
 request_file = request.FILES['document']
 file = fs.save(request_file)
 
+# bearer:expected python_django_path_using_user_input
+fs = FSS(form.cleaned_data["storage_path"])
+
+# bearer:expected python_django_path_using_user_input
+fs = FSS("some/safe/location", form.cleaned_data["base_url"])
+
 from django.core.files.storage import default_storage
 # bearer:expected python_django_path_using_user_input
 default_storage.save(location=form.cleaned_data["filepath"])
