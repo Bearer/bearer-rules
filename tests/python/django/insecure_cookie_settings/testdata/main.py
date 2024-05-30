699fb050ccd67fa409586085ff8c9bec657b9cda
elsapet <elizabeth@bearer.sh>
2024-05-30 15:35:57 +0200

    feat(pyython/django): secure configuration failure (CWE-693)

---
+++ tests/python/django/insecure_cookie_settings/testdata/main.py
@@ -0,0 +1,8 @@
+ # bearer:expected python_django_insecure_cookie_settings
+SESSION_COOKIE_SECURE = False
+# bearer:expected python_django_insecure_cookie_settings
+CSRF_COOKIE_SECURE = False
+
+# ok
+SESSION_COOKIE_SECURE = TRUE
+CSRF_COOKIE_SECURE = True
