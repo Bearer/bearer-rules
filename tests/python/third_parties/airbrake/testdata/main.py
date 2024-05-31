b53244db40a78efa5fbde3a71582bfa90838431f
elsapet <elizabeth@bearer.sh>
2024-05-31 12:29:10 +0200

    feat(python): add airbrake third parties rule

---
+++ tests/python/third_parties/airbrake/testdata/main.py
@@ -0,0 +1,12 @@
+import pybrake
+
+notifier = pybrake.Notifier()
+
+# bearer:expected python_third_parties_airbrake
+notifier.notify(user.email)
+# bearer:expected python_third_parties_airbrake
+notice = notifier.notify_sync(user.email)
+
+notice = notifier.build_notice()
+# bearer:expected python_third_parties_airbrake
+notice['params']['my_param'] = user.email
