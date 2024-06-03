3e4afc47f54a0760bb8978be03db37168bd51939
elsapet <elizabeth@bearer.sh>
2024-06-03 12:25:22 +0200

    fix: add safe case

--- tests/python/third_parties/airbrake/testdata/main.py
+++ tests/python/third_parties/airbrake/testdata/main.py
@@ -10,3 +10,7 @@ notice = notifier.notify_sync(user.email)
 notice = notifier.build_notice()
 # bearer:expected python_third_parties_airbrake
 notice['params']['my_param'] = user.email
+
+# ok
+notice['params']['my_param'] = user.uuid
+notice['params']['my_param'] = "some safe param"
