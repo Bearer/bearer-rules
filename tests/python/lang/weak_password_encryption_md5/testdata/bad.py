a119e4190cec26ed820e227c5276cc2e9e0018fd
elsapet <elizabeth@bearer.sh>
2024-06-05 16:49:56 +0200

    fix(python): password hashing rules

--- tests/python/lang/weak_password_encryption_md5/testdata/bad.py
+++ tests/python/lang/weak_password_encryption_md5/testdata/bad.py
@@ -13,3 +13,7 @@ password = user.password
 # bearer:expected python_lang_weak_password_encryption_md5
 result = hashlib.md5(password.encode())
 result.hexdigest()
+
+from hashlib import md5
+# bearer:expected python_lang_weak_password_encryption_md5
+result = md5(password.encode()).hexdigest()
