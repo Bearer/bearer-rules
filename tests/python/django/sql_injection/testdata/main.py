90cef70b75fd8da64a728a93defee3aafb084601
elsapet <elizabeth@bearer.sh>
2024-06-05 15:20:36 +0200

    feat(python): extend SQLi rules (#449)

--- tests/python/django/sql_injection/testdata/main.py
+++ tests/python/django/sql_injection/testdata/main.py
@@ -8,3 +8,6 @@ import pymysql
 safe = pymysql.connect().escape_string(user_input)
 User.objects.raw(f"SELECT * FROM x WHERE y = {safe}", [])
 
+from django.db.models.expressions import RawSQL
+# bearer:expected python_django_sql_injection
+RawSQL(f"SELECT * FROM x WHERE y = {user_input}")
