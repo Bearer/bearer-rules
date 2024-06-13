fa6f41445aa714fe4e8c2afdf1866503e176ee01
elsapet <elizabeth@bearer.sh>
2024-06-13 15:59:39 +0200

    feat(python): expand CWE 532 logger rule

--- tests/python/lang/logger/testdata/main.py
+++ tests/python/lang/logger/testdata/main.py
@@ -16,3 +16,21 @@ def bad3(user):
   myOtherLogger = something_else.getLogger(__name__)
   # bearer:expected python_lang_logger
   myOtherLogger.debug(f"User '{user.email}' logged")
+  
+  
+import my_custom
+def custom_bad():
+  logger = my_custom.get_logger()
+  # bearer:expected python_lang_logger
+  logger.info("changing password for %s", user.username)
+  
+def custom_bad2(self):
+    form = ResetPasswordEmailForm()
+    if form.validate_on_submit():
+        user = form.user
+        # bearer:expected python_lang_logger
+        logger.info('Password reset requested for user %s', user)
+        return redirect(url_for('.resetpass'))
+    return WPAuth.render_template('reset_password.html', form=form, identity=None, widget_attrs={},
+                                  email_sent=session.pop('resetpass_email_sent', False))
+  
