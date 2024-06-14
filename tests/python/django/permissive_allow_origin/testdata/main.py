0550f14b808f393ea0fd25cbcee25bd4140a3e61
elsapet <elizabeth@bearer.sh>
2024-06-14 12:02:34 +0200

    feat(python): extend allow-origin rules

--- tests/python/django/permissive_allow_origin/testdata/main.py
+++ tests/python/django/permissive_allow_origin/testdata/main.py
@@ -1,6 +1,6 @@
 # Use bearer:expected python_django_permissive_allow_origin to flag expected findings
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 response = HttpResponse()
 # bearer:expected python_django_permissive_allow_origin
@@ -8,6 +8,25 @@ response.headers['access-control-allow-origin'] = '*'
 
 # bearer:expected python_django_permissive_allow_origin
 HttpResponse(headers={"Access-Control-Allow-Origin": "*"})
+
+def bad(self, request, *args, **kwargs):
+  auth_header = request.headers.get('Authorization')
+  if auth_header:
+    method, token = auth_header.split(' ', 1)
+    if method != 'Bearer':
+      # bearer:expected python_django_permissive_allow_origin
+      return JsonResponse({
+        "error": "invalid_request",
+        "error_description": "Unknown authorization method"
+        }, status=400, headers={
+        'Access-Control-Allow-Origin': '*',
+      })
+    else:
+      # bearer:expected python_django_permissive_allow_origin
+      return HttpResponse(status=401, headers={
+        'WWW-Authenticate': 'Bearer realm="example"',
+        'Access-Control-Allow-Origin': '*',
+      })
 
 # ok
 response = HttpResponse()
