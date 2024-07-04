f4ac2b1396b03e89966f0fa81e73dea63cd92acf
elsapet <elizabeth@bearer.sh>
2024-07-04 16:36:07 +0200

    fix(golang): permissive regex rule for validation purposes (#457)

--- tests/go/lang/permissive_regex_validation/testdata/main.go
+++ tests/go/lang/permissive_regex_validation/testdata/main.go
@@ -5,11 +5,23 @@ import (
 	"regexp"
 )
 
-func bad() {
+func permissiveRegex(input string) {
+	re := regexp.MustCompile("[a-zA-Z0-9]*")
+
+	// likely not validation - we ignore this
+	re.ReplaceAllString(input, "foo")
+
 	// bearer:expected go_lang_permissive_regex_validation
-	_ = regexp.MustCompile("[a-zA-Z0-9]*")
+	re.Match([]byte(input))
+	// bearer:expected go_lang_permissive_regex_validation
+	re.MatchString(input)
 }
 
-func good() {
-	_ = regexp.MustCompile(`\A[a-zA-Z0-9]*\z`)
+func goodRegexp(input string) {
+	re := regexp.MustCompile(`\A[a-zA-Z0-9]*\z`)
+	if re.MatchString(input) {
+		// continue with valid string
+	} else {
+		// handle invalid string
+	}
 }
