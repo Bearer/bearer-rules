b6fe50410540ee4d8f82838240b4a47d02a68bf1
elsapet <elizabeth@bearer.sh>
2024-11-12 16:22:52 +0200

    fix(javascript): handle mysql pool as a promise (#467)

--- tests/javascript/lang/sql_injection/testdata/mysql2_sql_injection.js
+++ tests/javascript/lang/sql_injection/testdata/mysql2_sql_injection.js
@@ -2,31 +2,62 @@ const connection = mysql.createConnection({});
 const asyncConn = await mysql.createConnection({});
 
 module.exports.asyncFooBar = async function (req, res) {
-	// bearer:expected javascript_lang_sql_injection
-	await asyncConn.execute(
-		"SELECT * FROM `admin_users` WHERE ID = " + req.admin.id
-	);
-	res.send("ok");
+  // bearer:expected javascript_lang_sql_injection
+  await asyncConn.execute(
+    "SELECT * FROM `admin_users` WHERE ID = " + req.admin.id
+  );
+  res.send("ok");
 };
 
 module.exports.fooBar = function (req, _res) {
-// bearer:expected javascript_lang_sql_injection
-	connection.query(
-		"SELECT * FROM `user` WHERE name = " + req.params.customer.name
-	);
+  // bearer:expected javascript_lang_sql_injection
+  connection.query(
+    "SELECT * FROM `user` WHERE name = " + req.params.customer.name
+  );
 
-	// pool query
-	var pool = mysql.createPool();
-// bearer:expected javascript_lang_sql_injection
-	pool.query(
-		"SELECT * FROM users WHERE name = " + req.params.user_name,
-		function () {}
-	);
-	pool.getConnection(function (_err, conn) {
-// bearer:expected javascript_lang_sql_injection
-		conn.query("SELECT * FROM users WHERE name = " + req.params.user_name, function () {});
-		pool.releaseConnection(conn);
-	});
+  // pool query
+  var pool = mysql.createPool();
+  // bearer:expected javascript_lang_sql_injection
+  pool.query(
+    "SELECT * FROM users WHERE name = " + req.params.user_name,
+    function () {}
+  );
+  pool.getConnection(function (_err, conn) {
+    // bearer:expected javascript_lang_sql_injection
+    conn.query(
+      "SELECT * FROM users WHERE name = " + req.params.user_name,
+      function () {}
+    );
+    pool.releaseConnection(conn);
+  });
 
-	res.send("ok");
+  res.send("ok");
+};
+
+module.exports.asyncPool = async function (req, _res) {
+  // pool query
+  var pool = mysql.createPool({
+    host: "mysql",
+    user: "root",
+    password: "password",
+    database: "testdb",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  // promisify pool
+  const poolPromise = pool.promise();
+  var userName = req.query.name;
+  // bearer:expected javascript_lang_sql_injection
+  await poolPromise.query(`SELECT * FROM users WHERE name = '${userName}'`);
+  poolPromise.getConnection(function (_err, conn) {
+    // bearer:expected javascript_lang_sql_injection
+    conn.query(
+      `SELECT * FROM users WHERE name = '${userName}'`,
+      function () {}
+    );
+    poolPromise.releaseConnection(conn);
+  });
+
+  res.send("ok");
 };
